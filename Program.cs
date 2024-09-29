f758873f12cbaa7e29de89892f838e678de5c1a1
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-09-29 04:51:53 -0500

    coneccion BD

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using practica2.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("PostgressConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
